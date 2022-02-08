a44531577e63dcaff5ba0655be901dd5ad0896db
seyon <seyon.sivarajah@cambridgequantum.com>
2022-02-08 17:26:29 +0000

    use CodeGenModel for QIR generation

--- src/circuit.rs
+++ src/circuit.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 
 use inkwell::values::{BasicValueEnum, PointerValue};
-use pyqir_generator::interop::Model;
+use pyqir_generator::interop::{ClassicalRegister, CodeGenModel, QuantumRegister};
+use pyqir_generator::qir::instructions::{controlled, get_qubit, measure};
 use serde::ser::Error as SerdeError;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
@@ -80,7 +81,7 @@ impl<T: Serialize> Serialize for Patch<T> {
 /// Pytket specific models
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
-pub struct Register(pub String, pub Vec<i64>);
+pub struct Register(pub String, pub Vec<u64>);
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
 pub struct CompositeGate {
@@ -148,6 +149,9 @@ pub enum OpType {
     H,
     CX,
     Measure,
+    Rx,
+    Ry,
+    Rz,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
@@ -198,29 +202,39 @@ pub struct Circuit {
     pub implicit_permutation: Vec<Permutation>,
 }
 
-impl Model for Circuit {
+impl CodeGenModel for Circuit {
     fn name(&self) -> String {
         self.name.clone().unwrap_or("tket_circuit".to_string())
     }
 
     fn number_of_registers(&self) -> usize {
-        todo!()
+        self.registers().len()
     }
 
-    fn registers(&self) -> &Vec<pyqir_generator::interop::ClassicalRegister> {
-        todo!()
+    fn registers(&self) -> Vec<ClassicalRegister> {
+        let mut regmap = HashMap::new();
+        for bit in self.bits.iter() {
+            if let Some(size) = regmap.get_mut(&bit.0) {
+                *size = std::cmp::max(*size, bit.1[0] + 1);
+            } else {
+                regmap.insert(bit.0.clone(), bit.1[0] + 1);
+            }
+        }
+        regmap
+            .into_iter()
+            .map(|(name, size)| ClassicalRegister::new(name, size))
+            .collect()
     }
 
-    fn qubits(&self) -> &Vec<pyqir_generator::interop::QuantumRegister> {
-        todo!()
+    fn qubits(&self) -> Vec<QuantumRegister> {
+        self.qubits
+            .iter()
+            .map(|qb| QuantumRegister::new(qb.0.clone(), qb.1[0] as u64))
+            .collect()
     }
 
     fn static_alloc(&self) -> bool {
         true
-    }
-
-    fn initialize_registers(&self) -> bool {
-        false
     }
 
     fn write_instructions<'ctx>(
@@ -229,6 +243,152 @@ impl Model for Circuit {
         qubits: &HashMap<String, BasicValueEnum<'ctx>>,
         registers: &mut HashMap<String, Option<PointerValue<'ctx>>>,
     ) {
-        todo!()
+        for com in &self.commands {
+            emit(generator, com, qubits, registers);
+        }
+    }
+
+    fn number_of_qubits(&self) -> usize {
+        self.qubits.len()
+    }
+}
+
+pub(crate) fn emit<'ctx>(
+    generator: &qirlib::codegen::CodeGenerator<'ctx>,
+    com: &Command,
+    qubits: &HashMap<String, BasicValueEnum<'ctx>>,
+    registers: &mut HashMap<String, Option<PointerValue<'ctx>>>,
+) {
+    let qb_name = |reg: &Register| format!("{}{}", &reg.0[..], reg.1[0]);
+
+    let find_qubit = |reg: &Register| get_qubit(&qb_name(reg), qubits);
+
+    let optype = &com.op.op_type;
+    let params: Option<Vec<f64>> = com.op.params.as_ref().map(|params| {
+        params
+            .iter()
+            .map(|p| p.parse().expect("Could not parse parameter to float."))
+            .collect()
+    });
+    match optype {
+        OpType::CX => {
+            let control = find_qubit(&com.args[0]);
+            let qubit = find_qubit(&com.args[1]);
+            controlled(generator, generator.qis_cnot_body(), control, qubit);
+        }
+        OpType::H => {
+            generator.emit_void_call(generator.qis_h_body(), &[find_qubit(&com.args[0]).into()]);
+        }
+        OpType::Measure => {
+            measure(
+                generator,
+                &qb_name(&com.args[0]),
+                &qb_name(&com.args[1]),
+                // &format!("results"),
+                // &format!("results{}", &com.args[1].1[0]),
+                qubits,
+                registers,
+            );
+        }
+        // Instruction::Cz(inst) => {
+        //     let control = find_qubit(&inst.control);
+        //     let qubit = find_qubit(&inst.target);
+        //     controlled(
+        //         generator,
+        //         generator
+        //             .qis_cz_body()
+        //             .expect("qis_cz_body must be defined in the template"),
+        //         control,
+        //         qubit,
+        //     );
+        // }
+        // Instruction::Rx(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_rx_body()
+        //             .expect("qis_rx_body must be defined in the template"),
+        //         &[
+        //             generator.f64_to_f64(inst.theta),
+        //             find_qubit(&inst.qubit).into(),
+        //         ],
+        //     );
+        // }
+        // Instruction::Ry(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_ry_body()
+        //             .expect("qis_ry_body must be defined in the template"),
+        //         &[
+        //             generator.f64_to_f64(inst.theta),
+        //             find_qubit(&inst.qubit).into(),
+        //         ],
+        //     );
+        // }
+        OpType::Rz => {
+            generator.emit_void_call(
+                generator.qis_rz_body(),
+                &[
+                    generator.f64_to_f64(params.expect("Rz requires a parameter.")[0]),
+                    find_qubit(&com.args[0]).into(),
+                ],
+            );
+        }
+        // Instruction::S(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_s_body()
+        //             .expect("qis_s_body must be defined in the template"),
+        //         &[find_qubit(&inst.qubit).into()],
+        //     );
+        // }
+        // Instruction::SAdj(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_s_adj()
+        //             .expect("qis_s_adj must be defined in the template"),
+        //         &[find_qubit(&inst.qubit).into()],
+        //     );
+        // }
+        // Instruction::T(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_t_body()
+        //             .expect("qis_t_body must be defined in the template"),
+        //         &[find_qubit(&inst.qubit).into()],
+        //     );
+        // }
+        // Instruction::TAdj(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_t_adj()
+        //             .expect("qis_t_adj must be defined in the template"),
+        //         &[find_qubit(&inst.qubit).into()],
+        //     );
+        // }
+        // Instruction::X(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_x_body()
+        //             .expect("qis_x_body must be defined in the template"),
+        //         &[find_qubit(&inst.qubit).into()],
+        //     );
+        // }
+        // Instruction::Y(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_y_body()
+        //             .expect("qis_y_body must be defined in the template"),
+        //         &[find_qubit(&inst.qubit).into()],
+        //     );
+        // }
+        // Instruction::Z(inst) => {
+        //     generator.emit_void_call(
+        //         generator
+        //             .qis_z_body()
+        //             .expect("qis_z_body must be defined in the template"),
+        //         &[find_qubit(&inst.qubit).into()],
+        //     );
+        // }
+        _ => panic!("unsupported optype"),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 // mod basic_values;
 mod circuit;
 // mod emit;
-
+use pyqir_generator::emit::write_model_to_file;
 // use pyqir_generator::emitemit::{get_ir_string, write_circ_to_file};
 fn main() {
-    let circ_s = r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#;
+    let circ_s = r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]]], "op": {"params": ["0.2"], "type": "Rz"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#;
     let p: circuit::Circuit = serde_json::from_str(circ_s).unwrap();
-    // write_circ_to_file(&p, "dump.ll");
+    write_model_to_file(&p, "dump.ll");
     // dbg!(get_ir_string(&p));
 }
