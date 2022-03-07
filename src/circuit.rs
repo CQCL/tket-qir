c95947e389a22c97c33f06eaec5f580c4f7b51ec
seyon <seyon.sivarajah@cambridgequantum.com>
2022-03-07 18:42:20 +0000

    add if/then branching for conditional circbox
    tagged to commit https://github.com/CQCL/pyqir/commit/0f086ba541690f7a6a6b82cf4946f278aad9bc3d

--- src/circuit.rs
+++ src/circuit.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 
-use inkwell::values::{BasicValueEnum, PointerValue};
-use pyqir_generator::interop::{ClassicalRegister, CodeGenModel, QuantumRegister};
-use pyqir_generator::qir::instructions::{controlled, get_qubit, measure};
+use inkwell::values::{
+    BasicMetadataValueEnum, BasicValueEnum, FunctionValue, IntValue, PointerValue,
+};
+use qirlib::codegen::CodeGenerator;
+use qirlib::generation::interop::{ClassicalRegister, CodeGenModel, QuantumRegister};
+use qirlib::generation::qir::instructions::{controlled, get_qubit, measure};
 use serde::ser::Error as SerdeError;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
@@ -149,9 +152,14 @@ pub enum OpType {
     H,
     CX,
     Measure,
+    X,
+    Y,
+    Z,
     Rx,
     Ry,
     Rz,
+    Conditional,
+    CircBox,
 }
 
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq)]
@@ -237,27 +245,37 @@ impl CodeGenModel for Circuit {
         true
     }
 
-    fn write_instructions<'ctx>(
-        &self,
-        generator: &qirlib::codegen::CodeGenerator<'ctx>,
-        qubits: &HashMap<String, BasicValueEnum<'ctx>>,
-        registers: &mut HashMap<String, Option<PointerValue<'ctx>>>,
-    ) {
-        for com in &self.commands {
-            emit(generator, com, qubits, registers);
-        }
-    }
+    // fn write_instructions<'ctx>(
+    //     &self,
+    //     generator: &qirlib::codegen::CodeGenerator<'ctx>,
+    //     qubits: &HashMap<String, BasicValueEnum<'ctx>>,
+    //     registers: &mut HashMap<String, Option<PointerValue<'ctx>>>,
+    // ) {
+    // }
 
     fn number_of_qubits(&self) -> usize {
         self.qubits.len()
     }
+
+    fn write_instructions<'ctx>(
+        &self,
+        generator: &CodeGenerator<'ctx>,
+        qubits: &HashMap<String, BasicValueEnum<'ctx>>,
+        registers: &mut HashMap<String, Option<PointerValue<'ctx>>>,
+        entry_point: FunctionValue,
+    ) {
+        for com in &self.commands {
+            emit(generator, com, qubits, registers, entry_point);
+        }
+    }
 }
 
 pub(crate) fn emit<'ctx>(
-    generator: &qirlib::codegen::CodeGenerator<'ctx>,
+    generator: &CodeGenerator<'ctx>,
     com: &Command,
     qubits: &HashMap<String, BasicValueEnum<'ctx>>,
     registers: &mut HashMap<String, Option<PointerValue<'ctx>>>,
+    entry_point: FunctionValue,
 ) {
     let qb_name = |reg: &Register| format!("{}{}", &reg.0[..], reg.1[0]);
 
@@ -278,6 +296,15 @@ pub(crate) fn emit<'ctx>(
         }
         OpType::H => {
             generator.emit_void_call(generator.qis_h_body(), &[find_qubit(&com.args[0]).into()]);
+        }
+        OpType::X => {
+            generator.emit_void_call(generator.qis_x_body(), &[find_qubit(&com.args[0]).into()]);
+        }
+        OpType::Y => {
+            generator.emit_void_call(generator.qis_y_body(), &[find_qubit(&com.args[0]).into()]);
+        }
+        OpType::Z => {
+            generator.emit_void_call(generator.qis_z_body(), &[find_qubit(&com.args[0]).into()]);
         }
         OpType::Measure => {
             measure(
@@ -365,30 +392,123 @@ pub(crate) fn emit<'ctx>(
         //         &[find_qubit(&inst.qubit).into()],
         //     );
         // }
-        // Instruction::X(inst) => {
-        //     generator.emit_void_call(
-        //         generator
-        //             .qis_x_body()
-        //             .expect("qis_x_body must be defined in the template"),
-        //         &[find_qubit(&inst.qubit).into()],
-        //     );
-        // }
-        // Instruction::Y(inst) => {
-        //     generator.emit_void_call(
-        //         generator
-        //             .qis_y_body()
-        //             .expect("qis_y_body must be defined in the template"),
-        //         &[find_qubit(&inst.qubit).into()],
-        //     );
-        // }
-        // Instruction::Z(inst) => {
-        //     generator.emit_void_call(
-        //         generator
-        //             .qis_z_body()
-        //             .expect("qis_z_body must be defined in the template"),
-        //         &[find_qubit(&inst.qubit).into()],
-        //     );
-        // }
+        OpType::Conditional => {
+            let (condition_bit, args) = match &com.args[..] {
+                [a, b @ ..] => (a, b),
+                _ => panic!("Not enough args to conditional."),
+            };
+            let mut conditional = com.op.conditional.as_ref().unwrap().clone();
+            // for now only support conditional circbox
+            if let Some(OpBox::CircBox { .. }) = conditional.op.op_box.as_ref() {
+                ()
+            } else {
+                let newcirc = Circuit {
+                    name: None,
+                    phase: "".into(),
+                    commands: vec![Command {
+                        op: *conditional.op,
+                        args: args.to_vec(),
+                        opgroup: com.opgroup.clone(),
+                    }],
+                    qubits: args.to_vec(),
+                    // if inner op uses bits this will break
+                    bits: vec![],
+                    implicit_permutation: vec![],
+                };
+                conditional.op = Box::new(Operation {
+                    op_box: Some(OpBox::CircBox {
+                        circuit: newcirc,
+                        id: BoxID(uuid::Uuid::default()),
+                    }),
+                    op_type: OpType::CircBox,
+                    n_qb: None,
+                    params: None,
+                    signature: None,
+                    conditional: None,
+                });
+            }
+            emit_if(
+                generator,
+                registers,
+                qubits,
+                entry_point,
+                &conditional,
+                &qb_name(condition_bit),
+            )
+        }
         _ => panic!("unsupported optype"),
     }
+}
+
+fn emit_if<'ctx>(
+    generator: &CodeGenerator<'ctx>,
+    registers: &mut HashMap<String, Option<PointerValue<'ctx>>>,
+    qubits: &HashMap<String, BasicValueEnum<'ctx>>,
+    entry_point: FunctionValue,
+    conditional: &Conditional,
+    condition_bit: &String,
+) {
+    let inner_circ = if let Some(OpBox::CircBox { circuit, .. }) = conditional.op.op_box.as_ref() {
+        circuit
+    } else {
+        panic!("only works with CircBoxes.")
+    };
+
+    let comparison = match (conditional.width, conditional.value) {
+        (1, 1) => get_one(generator),
+        (1, 0) => get_zero(generator),
+        _ => panic!("only supports condtioning on one bit"),
+    };
+    let result = registers
+        .get(condition_bit)
+        .unwrap_or_else(|| panic!("Result {} not found.", condition_bit))
+        .unwrap_or_else(|| get_zero(generator));
+
+    let condition = equal(generator, result, comparison);
+    let then_block = generator.context.append_basic_block(entry_point, "then");
+    let else_block = generator.context.append_basic_block(entry_point, "else");
+    generator
+        .builder
+        .build_conditional_branch(condition, then_block, else_block);
+
+    let continue_block = generator
+        .context
+        .append_basic_block(entry_point, "continue");
+
+    let mut emit_block = |block, insts| {
+        generator.builder.position_at_end(block);
+        for inst in insts {
+            emit(generator, inst, qubits, registers, entry_point);
+        }
+
+        generator.builder.build_unconditional_branch(continue_block);
+    };
+
+    emit_block(then_block, &inner_circ.commands);
+    emit_block(else_block, &vec![]);
+    generator.builder.position_at_end(continue_block);
+}
+
+fn get_zero<'a>(generator: &CodeGenerator<'a>) -> PointerValue<'a> {
+    generator
+        .emit_call_with_return(generator.rt_result_get_zero(), &[], "zero")
+        .into_pointer_value()
+}
+
+pub(crate) fn get_one<'a>(generator: &CodeGenerator<'a>) -> PointerValue<'a> {
+    generator
+        .emit_call_with_return(generator.rt_result_get_one(), &[], "one")
+        .into_pointer_value()
+}
+
+pub(crate) fn equal<'a>(
+    generator: &CodeGenerator<'a>,
+    result1: PointerValue<'a>,
+    result2: PointerValue<'a>,
+) -> IntValue<'a> {
+    let result1 = BasicMetadataValueEnum::PointerValue(result1);
+    let result2 = BasicMetadataValueEnum::PointerValue(result2);
+    generator
+        .emit_call_with_return(generator.rt_result_equal(), &[result1, result2], "equal")
+        .into_int_value()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@
 // mod basic_values;
 mod circuit;
 // mod emit;
-use pyqir_generator::emit::write_model_to_file;
+use qirlib::generation::emit::ir;
 // use pyqir_generator::emitemit::{get_ir_string, write_circ_to_file};
-fn main() {
-    let circ_s = r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]]], "op": {"params": ["0.2"], "type": "Rz"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#;
+
+fn read_json_str(circ_s: &str) -> String {
     let p: circuit::Circuit = serde_json::from_str(circ_s).unwrap();
-    write_model_to_file(&p, "dump.ll");
+    ir(&p).unwrap()
+}
+fn main() {
+    println!(
+        "{}",
+        read_json_str(
+            r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]]], "op": {"params": ["0.2"], "type": "Rz"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#
+        )
+    );
     // dbg!(get_ir_string(&p));
+    println!(
+        "{}",
+        read_json_str(
+            r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "X"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["c", [0]], ["q", [1]]], "op": {"conditional": {"op": {"type": "Z"}, "value": 0, "width": 1}, "type": "Conditional"}}, {"args": [["c", [1]], ["q", [0]], ["q", [1]]], "op": {"conditional": {"op": {"box": {"circuit": {"bits": [], "commands": [{"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}, "id": "91810268-1b06-47b4-8609-992d066b56f2", "type": "CircBox"}, "type": "CircBox"}, "value": 1, "width": 1}, "type": "Conditional"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#
+        )
+    );
 }
