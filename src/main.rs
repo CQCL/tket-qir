8b40daec47afbc0a34c3580fd151ec072d90b194
Roland Guichard <roland.guichard@cambridgequantum.com>
2022-03-07 17:24:13 +0000

    Move the parsing logic to parse.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![allow(warnings, unused)]
 mod array1d;
 mod basic_values;
 mod circuit;
@@ -49,30 +50,6 @@ use std::io::Write;
 //     }
 // }
 
-
-
-
-fn match_call(instruction: &Instruction) -> Option<&llvm_ir::instruction::Call> {
-    match instruction {
-	Instruction::Call(call) => Some(call),
-	_ => None,
-    }
-}
-
-fn match_function(func: &Either<InlineAssembly, Operand>) -> Option<&llvm_ir::operand::Operand> {
-    match func {
-	Either::Right(operand) => Some(operand),
-	_ => None,
-    }
-}
-
-fn match_operand(operand: &Operand) -> Option<&llvm_ir::constant::ConstantRef> {
-    match operand {
-	Operand::ConstantOperand(const_ref) => Some(const_ref),
-	_ => None,
-    }
-}
-
 // fn match_globalref(const_ref: &Constant) -> Option<&llvm_ir::Constant> {
 //     match const_ref {
 // 	llvm_ir::Constant::GlobalReference(global_ref) => Some(global_ref),
@@ -84,47 +61,6 @@ fn match_operand(operand: &Operand) -> Option<&llvm_ir::constant::ConstantRef> {
 // fn match_globalref()
 
 
-fn match_to_optype(qir_optype: &str) -> Option<OpType> {
-
-    match qir_optype {
-	"h" => Some(OpType::H),
-	"cnot" => Some(OpType::CX),
-	"mz" => Some(OpType::Measure),
-	_ => None,
-    }
-
-}
-
-fn to_command(instruction: &Instruction) {
-    let mut func_signature = String::new();
-    if let Instruction::Call(call) = instruction {
-	// println!("{}", call);
-	if let Either::Right(operand) = &call.function {
-	    // println!("{}", operand);
-	    func_signature = operand.to_string();
-	}
-    }
-
-    let split_signature: Vec<&str> = func_signature.split("__").collect();
-    // println!("{:?}", split_signature);
-
-    let optype = match_to_optype(split_signature[3]).unwrap();
-
-    println!("{:?}", optype);
-
-    let mut args = String::new();
-    if let Instruction::Call(call) = instruction {
-	// println!("{}", call);
-	let arguments = &call.arguments;
-	if let Operand::ConstantOperand(operand) = &arguments[0].0 {
-	    // println!("{}", operand);
-	    args = operand.to_string();
-	}
-    }
-
-    println!("{:?}", args);
-}
-
 fn main() {
     let circ_s = r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#;
     let p: circuit::Circuit = serde_json::from_str(circ_s).unwrap();
@@ -134,52 +70,7 @@ fn main() {
     // write_circ_to_file(&p, "dump.ll");
     // dbg!(get_ir_string(&p));
 
-    let module = Module::from_bc_path("./example_files/SimpleGroverBaseProfile.bc").unwrap();
-    let first_function = &module.functions[0];
-    let first_basicblock = &first_function.basic_blocks[0];
-
-    let instructions = &first_basicblock.instrs;
-
-    println!("{:?}", instructions[1]);
-
-    to_command(&instructions[0]);
-
     
-    // let first_instruction = &first_basicblock.instrs[1];
-    // let call = match_call(first_instruction);
-    
-    // println!("{:?}", call.unwrap());
-    // let call_function = &call.unwrap().function;
-    // println!("{:?}", call_function);
-
-    // let operand = match_function(call_function).unwrap();
-    // let const_ref = match_operand(operand).unwrap();
-    // let global_ref = &*const_ref.as_ref().to_string();
-    // // println!("{:?}", global_ref);
-    // // println!("{:?}", global_ref.to_string());
-    
-    // let op: Vec<&str> = global_ref.split("__").collect();
-    // print!("{:?}", op[3]);
-    
-    // let params = &call.unwrap().arguments;
-    // println!("{:?}", params[0].0);
-
-    // let param_const_ref = match_operand(&params[0].0).unwrap();
-
-    // println!("{}", param_const_ref);
-
-
-    // let stuff: &str = &*param_const_ref.to_string();
-
-    // println!("{}", stuff);
-    
-    // if stuff.contains("Qubit") {
-    // 	println!("Qubit!");
-    // }
-
-    // if stuff.contains("null") {
-    // 	println!("null !")
-    // }
 
    
 }
